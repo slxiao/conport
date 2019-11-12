c482f4cbc6b088dcbd04fa1ad8d4dbe16b31ced2
Shelwin Xiao <562687381@qq.com>
2019-11-12 15:56:40 +0800

    test report

--- contport/build.py
+++ contport/build.py
@@ -1,6 +1,8 @@
 import jenkins
 import url
 
+from boundary import build_in_boundary
+
 def get_jenkins_home(job_url):
     myurl = url.parse(job_url)
     if myurl.port:
@@ -16,26 +18,30 @@ def get_jenkins_instance(job_url):
     jenkins_home = get_jenkins_home(job_url)
     return jenkins.Jenkins(jenkins_home)
 
-def get_build_numbers(jenkins_instance, job_name, fetch_all_builds=False):
-    result = jenkins_instance.get_job_info(job_name, fetch_all_builds=fetch_all_builds)
+def get_build_numbers(jenkins_instance, job_name):
+    result = jenkins_instance.get_job_info(job_name, fetch_all_builds=True)
     return [i["number"] for i in result["builds"]]
 
-def build_in_target():
-    pass
-
-def get_test_reports(job_url):
+def get_test_reports(job_url, past_hours):
     test_reports = {}
     jenkins_instance = get_jenkins_instance(job_url)
     job_name = get_job_name(job_url)
-    build_numbers = get_build_numbers(job_url)
+    build_numbers = get_build_numbers(jenkins_instance, job_name)
     for number in build_numbers:
-        report = jenkins_instance.get_build_test_report(job_name, number)
-        print(report)
-        
-
+        build_info = jenkins_instance.get_build_info(job_name, number)
+        if build_info["building"]:
+            continue
+        elif build_in_boundary(past_hours, build_info["timestamp"] + build_info["duration"]):
+            report = jenkins_instance.get_build_test_report(job_name, number)
+            test_reports[number] = {}
+            test_reports[number]["duration"] = build_info["duration"]
+            test_reports[number]["report"] = report
+        else:
+            break
+    return test_reports
 
 if __name__ == "__main__":
     #print(get_jenkins_home("http://xxx:8080/job/yyy/"))
     #print(get_jenkins_home("http://xxx/job/yyy/"))
     #print(get_job_name("http://xxx:8080/job/yyy/job/zzz"))
-    print(get_test_reports("http://10.183.40.203:49001/job/test-robot-framework/"))
+    print(get_test_reports("http://10.183.40.203:49001/job/test-robot-framework/", "24"))
