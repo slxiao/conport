54df194194564e78ac67bb29e6911b4a4a560c48
Shelwin Xiao <562687381@qq.com>
2019-11-18 18:16:54 +0800

    one_file_100%_coverage

--- tests/test_build.py
+++ tests/test_build.py
@@ -20,6 +20,8 @@ def test_build_in_boundary():
 def test_get_jenkins_home():
     assert build.get_jenkins_home(
         "http://127.0.0.1:8080/job/test-robot-framework/") == "http://127.0.0.1:8080"
+    assert build.get_jenkins_home(
+        "http://127.0.0.1/job/test-robot-framework/") == "http://127.0.0.1"
 
 
 def test_get_job_name():
@@ -36,3 +38,39 @@ def test_get_jenkins_instance(mocker):
     build.get_jenkins_instance("url")
     mocked_get.assert_called_once_with("url")
     mocked_jenkins.assert_called_once()
+
+
+def test_get_build_numbers(mocker):
+    mocked_jenkins_instance = mocker.Mock()
+    mocked_jenkins_instance.get_job_info.return_value = {
+        "builds": []}
+    assert build.get_build_numbers(mocked_jenkins_instance, "job") == []
+    mocked_jenkins_instance.get_job_info.assert_called_once_with("job")
+
+
+def test_get_test_reports(mocker):
+    mocked_jenkins_instance = mocker.Mock()
+    mocker.patch(
+        "conport.build.get_jenkins_instance", return_value=mocked_jenkins_instance)
+    mocker.patch("conport.build.get_job_name")
+    mocker.patch("conport.build.get_build_numbers")
+    assert build.get_test_reports("url", 24) == {}
+
+    mocker.patch("conport.build.get_build_numbers", return_value=[1, 2])
+    mocked_jenkins_instance.get_build_info.return_value = {
+        "building": True
+    }
+    assert build.get_test_reports("url", 24) == {}
+
+    mocked_jenkins_instance.get_build_info.return_value = {
+        "building": False,
+        "timestamp": 1,
+        "duration": 1
+    }
+    mocker.patch("conport.build.build_in_boundary", return_value=False)
+    assert build.get_test_reports("url", 24) == {}
+
+    mocker.patch("conport.build.build_in_boundary", return_value=True)
+    mocked_jenkins_instance.get_build_test_report.return_value = {}
+    assert build.get_test_reports("url", 24) == {
+        1: {'duration': 1, 'report': {}}, 2: {'duration': 1, 'report': {}}}
